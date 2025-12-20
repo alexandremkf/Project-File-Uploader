d1b533bdd65521c508e7ba0596d557b86d5a21e9
Alexandre Matiello <alexandrematiellokf@gmail.com>
2025-12-20 13:08:48 -0300

    Fiz a parte de Validação de arquivos (Multer), agora tem uma pequena validação dos arquivos subidos.

--- src/config/multer.js
+++ src/config/multer.js
@@ -13,6 +13,27 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "application/pdf",
+    "text/plain",
+  ];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Tipo de arquivo não permitido"));
+  }
+};
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
+  },
+  fileFilter,
+});
 
 export default upload;
--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,44 +1,55 @@
 import { Router } from "express";
-import multer from "multer";
 import prisma from "../config/prisma.js";
+import upload from "../config/multer.js";
 import { ensureAuthenticated } from "../middleware/auth.js";
 
 const router = Router();
 
-// Config do multer
-const upload = multer({
-  dest: "uploads/",
-});
-
 router.post(
   "/upload",
   ensureAuthenticated,
-  upload.single("file"),
-  async (req, res) => {
-    const { file } = req;
-    const { folderId } = req.body;
+  (req, res) => {
+    upload.single("file")(req, res, async (err) => {
+      const { folderId } = req.body;
 
-    console.log("folderId recebido:", folderId);
-    
-    if (!file) {
-      return res.redirect("/dashboard");
-    }
+      if (err) {
+        return res.render("dashboard", {
+          user: req.user,
+          folders: [],
+          files: [],
+          error: err.message,
+        });
+      }
 
-    await prisma.file.create({
-      data: {
-        name: file.originalname,
-        path: file.path,
-        size: file.size,
-        userId: req.user.id,
-        folderId: folderId || null,
-      },
+      if (!req.file) {
+        return res.render("dashboard", {
+          user: req.user,
+          folders: [],
+          files: [],
+          error: "Nenhum arquivo enviado",
+        });
+      }
+
+      const file = req.file;
+
+      console.log("folderId recebido:", folderId);
+
+      await prisma.file.create({
+        data: {
+          name: file.originalname,
+          path: file.path,
+          size: file.size,
+          userId: req.user.id,
+          folderId: folderId || null,
+        },
+      });
+
+      if (folderId) {
+        return res.redirect(`/folders/${folderId}`);
+      }
+
+      res.redirect("/dashboard");
     });
-
-    if (folderId) {
-    return res.redirect(`/folders/${folderId}`);
-    }
-
-    res.redirect("/dashboard");
   }
 );
 
